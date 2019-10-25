514914bed742332fb63ec247a9ce520d77c9c8b1
kubertest <sadbodh1@gmail.com>
2019-10-25 14:51:34 +0530

    path changed for log files

--- cifar10_train.py
+++ cifar10_train.py
@@ -108,8 +108,9 @@ def main():
     train_set_aug = Transform(train_set, [Crop(32, 32), FlipLR(), Cutout(data_aug_cutout_size, data_aug_cutout_size)])
     summary = train(model, lr_schedule, opt, train_set_aug, test_set, 
           batch_size=batch_size, loggers=(TableLogger(), TSV), timer=t, test_time_in_total=False, drop_last=True)
+    
       
-    with open('/mnt/share/results_job_id_'+str(2)+'.log', 'w') as csvfile:
+    with open('/mnt/share/temp/results_job_id_'+str(job_id)+'.log', 'w') as csvfile:
         print (os.getcwd())
         cw = csv.writer(csvfile, delimiter=',')
         for key, val in summary.items():
